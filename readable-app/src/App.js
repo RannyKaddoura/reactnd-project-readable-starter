63b181463cee86a2549bf125121be9a061358ea1
Frederik Trostorf <fredtrostorf@gmail.com>
2018-01-03 20:10:14 +0100

    refactor: Re organize components

---
+++ readable-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DefaultPage from './components/DefaultPage';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DefaultPage />
+      </div>
+    );
+  }
+}
+
+export default App;
