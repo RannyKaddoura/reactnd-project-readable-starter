d9d2614c30f1acda9b20633c63b32d7736278636
Frederik Trostorf <fredtrostorf@gmail.com>
2018-01-04 21:32:25 +0100

    Refactor: move store logic to app component

--- readable-app/src/app.js
+++ readable-app/src/app.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import HomePage from './components/pages/homepage/index.js';
-import * as API from './util/API';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './reducers';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 class App extends Component {
 
   render() {
     return (
-      <div>
+      <Provider store={store}>
         <HomePage />
-      </div>
+      </Provider>
     );
   }
 }
--- readable-app/src/index.js
+++ readable-app/src/index.js
@@ -4,19 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/css/index.css';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducer from './reducers';
-import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 registerServiceWorker();
