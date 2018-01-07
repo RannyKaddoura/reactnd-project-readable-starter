e3aed22870359d0d163cdd0f4c1c906e01dfd336
Frederik Trostorf <fredtrostorf@gmail.com>
2018-01-07 15:08:12 +0100

    feature: add simple form submission

--- readable-app/src/components/pages/post/components/comment-form.js
+++ readable-app/src/components/pages/post/components/comment-form.js
@@ -1,20 +1,72 @@
 import React from 'react';
 
 class CommentForm extends React.Component {
+  state = {
+    nameIsNotEmpty: true,
+    messageIsNotEmpty: true
+  };
 
-  render(){
+  onSubmit(event) {
+    event.preventDefault();
+
+    //validate form
+    if (this.validateForm()){
+
+      //handle further submission
+
+    }
+  }
+
+  /**
+   * simple form validation
+   * @returns {boolean}
+   */
+  validateForm() {
+    const nameIsNotEmpty = this.nameInput.value.trim().length > 0;
+    const messageIsNotEmpty = this.messageInput.value.trim().length > 0;
+
+    this.setState({
+      nameIsNotEmpty: nameIsNotEmpty,
+      messageIsNotEmpty: messageIsNotEmpty
+    });
+
+    return nameIsNotEmpty && messageIsNotEmpty;
+  }
+
+  render() {
+    console.log(this.state)
     return (
       <div>
-        <form>
-          <div class="form-group">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input type='text' className="form-control" id="name" />
+            <input
+              ref={i => (this.nameInput = i)}
+              type="text"
+              className={
+                'form-control ' +
+                (!this.state.nameIsNotEmpty ? 'is-invalid' : '')
+              }
+              id="name"
+            />
+            <div className="invalid-feedback">Required field.</div>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label htmlFor="message">Message:</label>
-            <textarea className="form-control" id="message" rows="3"></textarea>
+            <textarea
+              ref={t => (this.messageInput = t)}
+              className={
+                'form-control ' +
+                (!this.state.messageIsNotEmpty ? 'is-invalid' : '')
+              }
+              id="message"
+              rows="3"
+            />
+            <div className="invalid-feedback">Required field.</div>
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     );
