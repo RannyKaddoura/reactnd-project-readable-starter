4c3215da0ffce482e1e2026f8cea493689ff32da
Frederik Trostorf <fredtrostorf@gmail.com>
2018-01-13 10:29:37 +0100

    style: Add post controls

--- readable-app/src/components/pages/post/components/details.js
+++ readable-app/src/components/pages/post/components/details.js
@@ -10,11 +10,18 @@ export default function PostDetails(props) {
       <div className="row">
         <div className="col-sm-8 blog-main">
           <div className="blog-post">
-
             <div className="jumbotron">
               <h2>{post.title}</h2>
               <p className="blog-post-meta">
-                <strong>{post.author}</strong> {moment.unix(post.timestamp).format('MMM Do YYYY h:mm:ss a')}
+                <strong>{post.author}</strong>{' posted on '}
+                {moment.unix(post.timestamp).format('MMM Do YYYY h:mm:ss a')}
+                <i
+                  className={
+                    'fa ' +
+                    (post.voteScore < 0 ? 'fa-thumbs-o-down' : 'fa-thumbs-o-up')
+                  }
+                  aria-hidden="true"
+                />
               </p>
               <hr className="my-4" />
               <p className="blockquote">{post.body}</p>
@@ -30,7 +37,6 @@ export default function PostDetails(props) {
                     <Comment comment={comment} />
                     <hr className="my-1" />
                   </div>
-
                 ))}
               </div>
             )}
@@ -38,6 +44,36 @@ export default function PostDetails(props) {
             <CommentForm />
           </div>
         </div>
+        <aside className="col-sm-3 ml-sm-auto blog-sidebar">
+          <div className="sidebar-module sidebar-module-inset">
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a className="btn">
+                  <i className="fa fa-pencil-square-o" aria-hidden="true" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a className="btn">
+                  <i className="fa fa-times" aria-hidden="true" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="btn"
+                >
+                  <i className="fa fa-thumbs-o-up" aria-hidden="true" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="btn"
+                >
+                  <i className="fa fa-thumbs-o-down" aria-hidden="true" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
       </div>
     </div>
   );
