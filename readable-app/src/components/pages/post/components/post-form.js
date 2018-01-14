ffa366f8ab7ab5b7ac28003bb6490709b514d129
Frederik Trostorf <fredtrostorf@gmail.com>
2018-01-14 13:05:15 +0100

    feature: Form Validation

--- readable-app/src/components/pages/post/components/post-form.js
+++ readable-app/src/components/pages/post/components/post-form.js
@@ -2,47 +2,65 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm, formValueSelector } from 'redux-form';
 
+const required = value => {
+  return value ? undefined : 'Required field.';
+};
+
 class PostForm extends React.Component {
+  renderInput = ({ input, label, type, meta: { touched, error, warning } }) =>
+    (
+    <div className="form-group">
+      <label htmlFor="title">{label}</label>
+      <input
+        {...input}
+        className={'form-control' + (touched && error ? ' is-invalid' : '')}
+        type={type}
+        placeholder={label}
+      />
+      {touched && (error && <div className="invalid-feedback">{error}</div>)}
+    </div>
+  );
 
-
-
+  renderTextarea = ({
+    input,
+    label,
+    type,
+    meta: { touched, error, warning }
+  }) => (
+    <div className="form-group">
+      <label htmlFor="body">Body</label>
+      <textarea {...input} placeholder="Body" className="form-control" />
+    </div>
+  );
 
   render() {
-
-    const {handleSubmit, categories} = this.props;
+    const { handleSubmit, categories } = this.props;
     return (
       <form onSubmit={handleSubmit}>
         <h1 className="display-4">Create Post</h1>
         <hr className="my-4" />
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <Field
-            name="title"
-            component="input"
-            type="text"
-            placeholder="Title"
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="body">Body</label>
-          <Field
-            name="body"
-            component="textarea"
-            placeholder="Body"
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="title">Author</label>
-          <Field
-            name="author"
-            component="input"
-            type="text"
-            placeholder="Author"
-            className="form-control"
-          />
-        </div>
+
+        <Field
+          name="title"
+          component={this.renderInput}
+          type="text"
+          label="Title"
+          validate={[required]}
+        />
+
+        <Field
+          name="body"
+          component={this.renderTextarea}
+          label="Body"
+        />
+
+        <Field
+          name="author"
+          component={this.renderInput}
+          type="input"
+          label="Author"
+        />
+
         <div className="form-group">
           <label htmlFor="exampleFormControlSelect1">Category</label>
           <Field name="category" component="select" className="form-control">
@@ -54,9 +72,7 @@ class PostForm extends React.Component {
             ))}
           </Field>
         </div>
-        <button className="btn btn-primary">
-          Save
-        </button>
+        <button className="btn btn-primary">Save</button>
         <button type="button" className="btn btn-secondary">
           Cancel
         </button>
@@ -70,5 +86,8 @@ PostForm = reduxForm({
   enableReinitialize: true
 })(PostForm);
 
-const mapsStateToProps = ({ post, categories }) => ({ initialValues: post, categories });
+const mapsStateToProps = ({ post, categories }) => ({
+  initialValues: post,
+  categories
+});
 export default connect(mapsStateToProps)(PostForm);
